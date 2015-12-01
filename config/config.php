ac5bec9640067270331fc46365f02238e9a8e8f8
yipeng <huang.ypeng@gmail.com>
2015-12-01 15:42:57 +0100

    refa

--- config/config.php
+++ config/config.php
@@ -2,15 +2,15 @@
 
 define('BASE_URL', 'http://yipengpm.alwaysdata.net');
 
-define('SMTP_HOST', 'smtp-yipengpm.alwaysdata.net');
-define('SMTP_PORT', 587);
-define('SMTP_USERNAME', 'yipengpm@alwaysdata.net');
-define('SMTP_PASSWORD', 'Hyp613090');
-define('EMAIL_FROM', 'olic2016@utt.fr');
+define('SMTP_HOST',       'smtp-yipengpm.alwaysdata.net');
+define('SMTP_PORT',       587);
+define('SMTP_USERNAME',   'yipengpm@alwaysdata.net');
+define('SMTP_PASSWORD',   'Hyp613090');
+define('EMAIL_FROM',      'olic2016@utt.fr');
 define('EMAIL_FROM_NAME', 'OLIC2016');
-define('EMAIL_ADMIN', 'huang.ypeng@gmail.com');
+define('EMAIL_ADMIN',     'huang.ypeng@gmail.com');
 
-define('DB_HOST', 'mysql-yipengpm.alwaysdata.net');
+define('DB_HOST',     'mysql-yipengpm.alwaysdata.net');
 define('DB_USERNAME', 'yipengpm');
 define('DB_PASSWORD', 'Hyp613090');
-define('DB_NAME', 'yipengpm_olic');
+define('DB_NAME',     'yipengpm_olic');
