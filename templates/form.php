81ac44482d31909d0dfc573b0ccad1e2d5bd2aef
yipeng <huang.ypeng@gmail.com>
2015-12-03 10:46:34 +0100

    add notice about personal info

--- templates/form.php
+++ templates/form.php
@@ -4,6 +4,21 @@
         <div class="alert alert-info">
             Les champs marqu&eacute;s par <span class="mandat">*</span> sont obligatoires
         </div>
+        <div class="panel panel-default" style="padding:10px;text-align:justify;">
+            <em style="font-size:0.85em;">
+            Les informations recueillies font l'objet d'un traitement informatique 
+            destin&eacute; &agrave; la gestion g&eacute;n&eacute;rale du colloque par les diff&eacute;rents 
+            organisateurs. En aucun cas les informations vous concernant ne seront 
+            transmises &agrave; des tiers. Dans le cadre de colloques organis&eacute;s par 
+            l'Universit&eacute;, le pr&eacute;sident est alors le responsable du traitement de ces 
+            donn&eacute;es. Les destinataires des donn&eacute;es sont : les gestionnaires du 
+            colloque. Conform&eacute;ment &agrave; la loi "Informatique et Libert&eacute;s", vous 
+            b&eacute;n&eacute;ficiez d'un droit d'acc&egrave;s et de rectification aux informations qui 
+            vous concernent. Si vous souhaitez exercer ce droit et obtenir 
+            communication des informations vous concernant, veuillez vous adresser 
+            au responsable du colloque.
+            </em>
+        </div>
         <div class="form-group col-md-6">
             <label>Nom:<sup>*</sup></label>
             <input class="form-control" type="text" name="lastname" required>
@@ -17,7 +32,7 @@
             <input class="form-control" type="text" name="affiliation" required>
         </div>
         <div class="form-group col-md-12">
-            <label>Adresse professionelles:</label>
+            <label>Adresse professionelle:</label>
             <input class="form-control" type="text" name="address">
         </div>
         <div class="form-group col-md-12">
@@ -45,7 +60,7 @@
         <div class="checkbox">
             <label>
                 <input type="checkbox" name="events[]" value="0">
-                Participation &agrave; la sesson des pr&eacute;sentations le matin
+                Participation &agrave; la session des pr&eacute;sentations le matin
             </label>
         </div>
         <div class="checkbox">
